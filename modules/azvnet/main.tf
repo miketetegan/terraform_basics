afe12782d25814b3db596c09895d569473c6993d
Mike Tetegan-Benissan <159888041+miketetegan@users.noreply.github.com>
2024-07-13 20:31:15 +0000

    Update main.tf

--- modules/azvnet/main.tf
+++ modules/azvnet/main.tf
@@ -1,3 +1,16 @@
 // Contains the actual resources you want to create 
 
-resoure 
+resource "azurerm_virtual_network" "lab-vnet" {
+  name                = var.vnet_name
+  location            = var.resource_location  // Define the location variable specified
+  resource_group_name = var.resource_group   //Define the rg variable specified 
+  address_space       = var.address_space_list
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet"
+  resource_group_name  = var.resource_group   //Define the rg variable specified 
+  virtual_network_name = ${var.vnet_name}
+  address_prefixes     = var.address_prefixes_list
+}
+
