0dca522b9dd258f76c88ff87da393f4bb6dfef83
Mike Tetegan-Benissan <159888041+miketetegan@users.noreply.github.com>
2024-07-13 23:09:57 +0000

    Update main.tf

--- config/main.tf
+++ config/main.tf
@@ -7,6 +7,17 @@
 // You can use the existing resources as the values of the modules variables like azurerm_virtual_network.lab-vnet.id
 
 
+// Define a remote state 
+terraform {
+  required_version = ">= 1.7"
+  backend "azurerm" {
+    resource_group_name  = "StorageAccount-ResourceGroup"  # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
+    storage_account_name = "abcd1234"                      # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    container_name       = "tfstate"                       # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+    key                  = "prod.terraform.tfstate"        # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+  }
+}
+
 
 // Configure the Microsoft Azure Provider
 provider "azurerm" {
