d00df4b2d15de057581721917b87722a6ee49150
Mike Tetegan-Benissan <159888041+miketetegan@users.noreply.github.com>
2024-07-13 20:34:39 +0000

    Update variables.tf

--- modules/azvnet/variables.tf
+++ modules/azvnet/variables.tf
@@ -1,1 +1,16 @@
+// Define the variables specified in the module 
+
+variable "vnet_name" {}
+
+variable "resource_location" {}
+
+variable "resource_group" {}
+
+variable "address_space_list" {
+  type = list
+}
+
+variable "address_prefixes_list" {
+  type = list
+}
 
